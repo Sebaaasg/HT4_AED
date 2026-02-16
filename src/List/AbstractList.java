9f07633a65009edacd7b1c67444fde2d6d771188
ricardo18ec <richiescobarc18@gmail.com>
2026-02-16 07:45:54 -0600

    Refactoriza AbstractList para mejorar manejo de tamaño

--- src/List/AbstractList.java
+++ src/List/AbstractList.java
@@ -1,20 +1,29 @@
 package List;
 
 public abstract class AbstractList<T> implements IList<T> {
-    
-    protected int size; // Accesible por las hijas
+
+    protected int count;
 
     public AbstractList() {
-        this.size = 0;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
+        this.count = 0;
     }
 
     @Override
     public int size() {
-        return size;
+        return count;
     }
+
+    @Override
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    @Override
+    public abstract void addFirst(T value);
+
+    @Override
+    public abstract T removeFirst();
+
+    @Override
+    public abstract T getFirst();
 }
