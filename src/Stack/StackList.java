76c5dfcea142e12df236353d21115d9bb2cc0a34
ricardo18ec <richiescobarc18@gmail.com>
2026-02-16 07:46:43 -0600

    Agrega validación de tipo de lista en StackList

--- src/Stack/StackList.java
+++ src/Stack/StackList.java
@@ -1,32 +1,38 @@
 package Stack;
 
+import List.DoublyLinkedList;
 import List.IList;
 import List.SinglyLinkedList;
-import List.DoublyLinkedList;
 
 // Hace que una lista se comporte como una pila
 public class StackList<T> extends AbstractStack<T> {
 
-    private IList<T> internalList; // La lista real que guarda los datos
+    private IList<T> internalList;// La lista real que guarda los datos
 
     // Constructor: Recibe "1" o "2"
     public StackList(String listType) {
-        if (listType.equals("2")) {
-            this.internalList = new DoublyLinkedList<>();
+
+        if (listType == null) {
+            throw new IllegalArgumentException("List type cannot be null");
+        }
+
+        if (listType.equalsIgnoreCase("singly")) {
+            internalList = new SinglyLinkedList<>(); // Por defecto usamos la Simple
+        } else if (listType.equalsIgnoreCase("doubly")) {
+            internalList = new DoublyLinkedList<>();
         } else {
-            // Por defecto usamos la Simple
-            this.internalList = new SinglyLinkedList<>();
+            throw new IllegalArgumentException("Invalid list type");
         }
     }
 
     @Override
     public void push(T item) {
-        internalList.addFirst(item); // Pila LIFO 
+        internalList.addFirst(item); //Pila LIFO
     }
 
     @Override
     public T pop() {
-        return internalList.removeFirst(); // Sacamos del inicio
+        return internalList.removeFirst(); //Sacamos al inicio
     }
 
     @Override
